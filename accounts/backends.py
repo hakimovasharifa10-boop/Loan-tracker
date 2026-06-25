3a6ff3a7ff5744b9bb767bdb300a5de9d89f1f46
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:34:03 +0500

    seventh commit

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,2 +1,16 @@
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+from django.contrib.auth import get_user_model
 
-        
+User = get_user_model()
+
+class EmailOrUsernameBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(Q(username=username) | Q(email=username))
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return user
+        return None
