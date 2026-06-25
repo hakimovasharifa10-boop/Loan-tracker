8780301e5a61dbad79e275b0a868c33efd7e1863
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:44:22 +0500

    sixteenth commit

--- loans/views.py
+++ loans/views.py
@@ -11,6 +11,19 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv('GROQ_API'))
 
+def get_exchange_rates():
+    # Берём актуальный курс валют
+    try:
+        response = requests.get('https://api.exchangerate-api.com/v4/latest/TJS', timeout=5)
+        data = response.json()
+        return {
+            'USD': round(data['rates']['USD'], 4),
+            'RUB': round(data['rates']['RUB'], 2),
+            'EUR': round(data['rates']['EUR'], 4),
+        }
+    except:
+        return {'USD': 0.092, 'RUB': 8.5, 'EUR': 0.085}
+
     
         
         
