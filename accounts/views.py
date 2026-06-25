5566409d482a845ee9dc0515bf24ae80712ffdc4
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:37:09 +0500

    tenth commit

--- accounts/views.py
+++ accounts/views.py
@@ -19,4 +19,20 @@ def send_confirmation_code(user):
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[user.email]
     )
+    
+def register_view(request):
+    if request.method == 'POST':
+        username  = request.POST.get('username')
+        email     = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if not username or not email or not password1:
+            return render(request, 'accounts/register.html', {'error': 'All fields are required!'})
+        elif password1 != password2:
+            return render(request, 'accounts/register.html', {'error': 'Passwords do not match!'})
+        elif User.objects.filter(username=username).exists():
+            return render(request, 'accounts/register.html', {'error': 'Username already exists!'})
+        elif User.objects.filter(email=email).exists():
+            return render(request, 'accounts/register.html', {'error': 'Email already exists!'})
 
