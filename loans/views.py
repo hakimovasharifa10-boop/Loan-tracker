31b20b9de5ff34699124cace5ce06952e7795266
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:43:22 +0500

    fifteenth commit

--- loans/views.py
+++ loans/views.py
@@ -1,4 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Loan
+from groq import Groq
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+client = Groq(api_key=os.getenv('GROQ_API'))
 
     
         
