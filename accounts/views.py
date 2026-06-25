75035923a33b1022f8d8fd6e629f8cea4021e115
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:38:31 +0500

    eleventh commit

--- accounts/views.py
+++ accounts/views.py
@@ -36,3 +36,36 @@ def register_view(request):
         elif User.objects.filter(email=email).exists():
             return render(request, 'accounts/register.html', {'error': 'Email already exists!'})
 
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password1,
+            is_active=False
+        )
+        send_confirmation_code(user)
+        return render(request, 'accounts/confirm.html', {'user': user})
+
+    return render(request, 'accounts/register.html')
+
+def confirm_email(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        code  = request.POST.get('code')
+
+        user = User.objects.filter(email=email).first()
+        if not user:
+            return render(request, 'accounts/confirm.html', {'error': 'Incorrect email!'})
+
+        try:
+            email_confirm = EmailConfirm.objects.get(user=user)
+        except EmailConfirm.DoesNotExist:
+            return render(request, 'accounts/confirm.html', {'error': 'Code not found!'})
+
+        if code != email_confirm.code:
+            return render(request, 'accounts/confirm.html', {'error': 'Incorrect code!'})
+        
+        user.is_active = True
+        user.save()
+        return redirect('login')
+
+    return render(request, 'accounts/confirm.html')
