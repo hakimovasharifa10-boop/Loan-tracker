54cf5ee8c57a28ae608c121b6e58aed8d101fac8
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:53:00 +0500

    twenty-second commit

--- payments/views.py
+++ payments/views.py
@@ -1,2 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Payment
+from loans.models import Loan
 
+@login_required
+def payment_list(request):
+    payments = Payment.objects.filter(user=request.user)
+
+    loan_id = request.GET.get('loan')
+    if loan_id:
+        payments = payments.filter(loan__id=loan_id)
+
+    
+    loans = Loan.objects.filter(user=request.user)
+
+
+    total = sum(p.amount for p in payments)
+    
+    return render(request, 'payments/payment_list.html', {
+        'payments':      payments,
+        'loans':         loans,
+        'total':         total,
+        'selected_loan': loan_id,
+    })
