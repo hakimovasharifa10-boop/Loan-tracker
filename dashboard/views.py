f0fac41d49177435f75270df2940cb920a985b20
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-26 00:00:47 +0500

    twenty-seventh commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from loans.models import Loan
+from payments.models import Payment
+import requests
 
-# Create your views here.
+def get_exchange_rates():
+    try:
+        response = requests.get(
+            'https://api.exchangerate-api.com/v4/latest/TJS',
+            timeout=5
+        )
+        data = response.json()
+        return {
+            'USD': round(data['rates']['USD'], 4),
+            'RUB': round(data['rates']['RUB'], 2),
+            'EUR': round(data['rates']['EUR'], 4),
+        }
+    except:
+        return {'USD': 0.092, 'RUB': 8.5, 'EUR': 0.085}
