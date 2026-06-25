ff69204acfff577f38a744503c99838b61130e16
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:35:00 +0500

    eighth commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.core.mail import send_mail
+from django.conf import settings
+from random import randint
+from .models import EmailConfirm
 
-
+User = get_user_model()
 
