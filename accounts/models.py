dc6b1dfa3b21fcb081f8984a92e4b52fc8f9d427
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-25 23:23:56 +0500

    second commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class EmailConfirm(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='email_confirm')
+    code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.code}'
     
     
