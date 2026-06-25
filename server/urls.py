fd19e35fd75065f5834685126e71859d1a73760f
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-26 00:04:46 +0500

    thirty

--- server/urls.py
+++ server/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+
+    path('', lambda request: redirect('login')),
+
+    # Приложения
+    path('accounts/',  include('accounts.urls')),
+    path('loans/',     include('loans.urls')),
+    path('payments/',  include('payments.urls')),
+    path('dashboard/', include('dashboard.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
