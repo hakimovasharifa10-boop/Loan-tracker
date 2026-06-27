17935851732a2ab95610d095d5cd61c343f1e8b4
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-27 07:09:50 +0500

    form

---
+++ payments/form.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Payment
+
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['loan', 'amount', 'date', 'notes']
+        widgets = {
+            'loan':   forms.Select(attrs={'class': 'form-input'}),
+            'amount': forms.NumberInput(attrs={
+                'class': 'form-input',
+                'placeholder': '1500',
+                'step': '0.01',
+            }),
+            'date':   forms.DateInput(attrs={
+                'class': 'form-input',
+                'type': 'date',
+            }),
+            'notes':  forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'e.g. June payment',
+            }),
+        }
+        labels = {
+            'loan':   'Loan',
+            'amount': 'Amount (TJS)',
+            'date':   'Payment Date',
+            'notes':  'Notes (optional)',
+        }
+
+    def __init__(self, *args, **kwargs):
+        # Передаём пользователя чтобы показывать только его кредиты
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['loan'].queryset = user.loans.all()
