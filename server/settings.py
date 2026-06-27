c85304e74a7ac8125b2c1d8589ee2aec08d4c023
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-06-27 06:30:33 +0500

    add

--- server/settings.py
+++ server/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'loans',
     'payments',
     'dashboard',
-    'goals'
+    'goals',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
