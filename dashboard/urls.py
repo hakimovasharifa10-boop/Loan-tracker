3bc674d5f8ed14cf4a256ddd774eba85261774ce
hakimovasharifa10-boop <hakimovasharifa10@gmail.com>
2026-07-02 22:55:51 +0500

    file

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
     path('download-report/', views.download_report, name='download_report'),
     path('send-test-reminder/', views.send_test_reminder, name='send_test_reminder'),
+    path('send-payment-reminder/', views.send_payment_reminder, name='send_payment_reminder'),
 ]
-
